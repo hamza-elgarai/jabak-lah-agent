21e4af5272529d4c68f6b2f0b5a18c22b2e7174f
hamza-elgarai <hamzaelgarai10@gmail.com>
2023-06-08 01:38:37 +0100

    added auth to getClients

--- src/app/service/clients.service.ts
+++ src/app/service/clients.service.ts
@@ -2,15 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisterBody } from '../interfaces/RegisterBody';
 import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientRegisterService {
 
-  constructor(private http:HttpClient,private router:Router) { }
+  constructor(private http:HttpClient,private auth:AuthService,private router:Router) { }
 
   registerClient(registerBody:RegisterBody){
-    return this.http.post('http://localhost:8090/client/auth/register',registerBody)
+    return this.http.post('http://localhost:8090/client/auth/register',registerBody,{
+      headers:{
+        "Authorization": "Bearer "+this.auth.getToken()
+      }
+    })
   }
 }
